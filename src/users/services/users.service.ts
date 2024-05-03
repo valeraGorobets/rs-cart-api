7cb86bcc757e46628f0a260d10073fc2de2c8db9
Valera Gorobets <valera.gorobets@gmail.com>
2024-05-03 11:56:45 +0300

    revert users.service.ts

--- src/users/services/users.service.ts
+++ src/users/services/users.service.ts
@@ -9,20 +9,7 @@ export class UsersService {
   private readonly users: Record<string, User>;
 
   constructor() {
-    this.users = {
-      '2e0c53d8-89a4-4b5b-b8e3-20a74d173f63': {
-        id: '2e0c53d8-89a4-4b5b-b8e3-20a74d173f63',
-        name: 'John Doe',
-        password: 'lalala',
-        email: 'johndoe@gmail.com',
-      },
-      '5d5e9a38-4b97-4b20-aaf7-d40c16502989': {
-        id: '5d5e9a38-4b97-4b20-aaf7-d40c16502989',
-        name: 'Mike Smith',
-        password: 'gogogo',
-        email: 'mikesmith@gmail.com',
-      }
-    }
+    this.users = {}
   }
 
   findOne(userId: string): User {
